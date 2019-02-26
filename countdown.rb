c7c3ac76ae6604032e93e1f7639d8a8d24114939
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-26 18:11:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(number)
   number = 10 
   while number > 1 
-    print "#{number} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
-    print "HAPPY NEW YEAR!"
+    puts "HAPPY NEW YEAR!"
 end
